01098cad38bbd41c7c1f7ec89a483ad178ab29ef
k-omkar <om.kulkarni94@gmail.com>
2020-04-28 01:37:47 +0200

    Added post details
    Signed-off-by: k-omkar <om.kulkarni94@gmail.com>

--- src/main/java/blog/controllers/PostController.java
+++ src/main/java/blog/controllers/PostController.java
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PostController{
 
+    @Autowired
+    private PostService postService;
 
     @RequestMapping("/posts/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
+        Post post = postService.findById(id);
+        model.addAttribute("post", post);
         return "posts/view";
     }
 
