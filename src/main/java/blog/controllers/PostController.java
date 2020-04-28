c79ac79907ac76840df9e975522b335b49d04f2a
k-omkar <om.kulkarni94@gmail.com>
2020-04-28 20:51:12 +0200

    Added redirect for errors
    Signed-off-by: k-omkar <om.kulkarni94@gmail.com>

--- src/main/java/blog/controllers/PostController.java
+++ src/main/java/blog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package blog.controllers;
 
 import blog.models.Post;
+import blog.services.NotificationService;
 import blog.services.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,16 @@ public class PostController{
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private NotificationService notifyService;
+
     @RequestMapping("/posts/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
+        if(post == null) {
+            notifyService.addErrorMessage("Post #" + id + " not found please check again");
+            return "redirect:/";
+        }
         model.addAttribute("post", post);
         return "posts/view";
     }
