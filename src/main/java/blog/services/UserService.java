de822b19944b72c57c700c8b73ad2b4ea07685be
k-omkar <om.kulkarni94@gmail.com>
2020-04-28 23:50:16 +0200

    update to include login options
    Signed-off-by: k-omkar <om.kulkarni94@gmail.com>

--- src/main/java/blog/services/UserService.java
+++ src/main/java/blog/services/UserService.java
@@ -5,6 +5,8 @@ import blog.models.User;
 import java.util.List;
 
 public interface UserService {
+    boolean authenticate(String username, String password);
+
     List<User> findAll();
     User findById(Long id);
     User create(User user);
--- src/main/java/blog/services/UserServiceImplementation.java
+++ src/main/java/blog/services/UserServiceImplementation.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserServiceImplementation implements UserService{
     @Autowired
     private UserRepository userRepo;
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        return Objects.equals(username, password);
+    }
 
     public List<User> findAll(){
         return this.userRepo.findAll();
